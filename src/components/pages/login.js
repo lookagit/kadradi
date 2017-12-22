b50d4c60c340b44cf2b17887728c8c49bd304efa
Your Name <you@example.com>
2017-12-22 12:28:21 +0100

    log sa fb-om

--- src/components/pages/login.js
+++ src/components/pages/login.js
@@ -6,12 +6,20 @@ import FacebookLogin from 'react-facebook-login';
 const responseFacebook = (response) => {
   console.log(response);
 }
+
 class Login extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      user: []
+    }
+  }
+
     render(){
         return(
             <div>
               <FacebookLogin
-                appId="1088597931155576"
+                appId="350084062068574"
                 autoLoad={true}
                 fields="name,email,picture"
                 callback={responseFacebook} />
